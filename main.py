e98bdc3cdc6cb97891e183112a7154ae91224dc4
Ruslann2013 <sitdykovruslan55@gmail.com>
2024-05-11 12:26:36 +0300

    change оdd number

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ a = {
     'player': 0,
     'computer': 0
 }
+
+def is_odd(n , n1):
+    if n % 2 == 0 and n1 % 2 == 0:
+        return False
+    else:
+        return True
+
 while True:
     b = random.randint(10, 120)
     c = random.randint(10, 120)
+    if not is_odd(b , c):
+        continue
     op = random.choice(('+', '-'))
     if op == '-':
         ans = b - c
